e54dabebad25d9343dbab1db947913dc98d0ecc5
John Crawford <john.crawford@crawfishpress.com>
2022-07-29 15:42:21 -0400

    added a circular constraint

--- src/demo_verlet.py
+++ src/demo_verlet.py
@@ -4,9 +4,10 @@ Based on video:
   - How to program a basic Physic Engine by Pezzza's Work
   - https://www.youtube.com/watch?v=lS_qeBy3aQI
 
-Note: The Kivy Widget I'm using to display circles, has a Position,
+Note: The Kivy Widget I'm using to display balls, has a Position,
 but the canvas-circle (or Ellipse) has its own Position that determines
 where it will be displayed. Widgets are separate from what the canvas draws.
+So you will see an offset applied to the Circle.
 
 Note: Widget positions (self.pos) are tuples (5, 5), so I added a converter
 on the Vector2 dataclass, the iterator method '__iter__()'.
@@ -16,6 +17,22 @@ only without the asterisk.
 
 Note: Kivy coordinates - Y=0, at bottom of screen
 So acceleration is reversed.
+
+Note: Adding constraints - there is currently only one, an "invisible"
+circle - so adding a Kivy widget to make it visible.
+
+Note: adding collisions between balls. My code is slightly different from
+the video - I don't have a collection of Verlet objects, I have a collection
+of Kivy Widgets (circles), each of which has its own Verlet object. While
+this works fine, for purposes of falling/constraints - for purposes of
+detecting collisions between balls, there has to be an iterator-function
+at a different level than the Verlet object.
+
+I'm not entirely sure from the video, but it looks like it might be using
+an Entity-Component-System (or possibly that's build-in to C++).
+
+Anyway - the collision-code isn't exactly like the video...
+
 """
 
 from kivy.app import App
@@ -32,17 +49,38 @@ Window.left = 400
 window_size = (1600, 1000)
 
 # Random engine constants
-base_gravity = Vector2(0.0, -0.5)
-delta_t: float = 0.2
+base_gravity = Vector2(0.0, -2)
+delta_t: float = 0.5
+
+pit_constraints = {
+    'pit_pos': (0, 0),
+    'pit_size': (1000, 1000),
+    'pit_radius': 500.0,
+    'pit_color': (0.5, 0.5, 0.5, 1.0),
+    'ball_radius': 50,  # Derived from circle-size below, could change
+}
 
 
-class SomeCircle(Widget):
-    def __init__(self, start_pos: tuple, rgb_tuple: tuple, **kwargs):
+class BallPit(Widget):
+    def __init__(self, constraints: dict, **kwargs):
+        super().__init__(**kwargs)
+        self.pos: tuple = constraints['pit_pos']
+        self.size = constraints['pit_size']
+        self.color: tuple = constraints['pit_color']
+
+        with self.canvas.before:
+            Color(*self.color)
+            self.circle = Ellipse(pos=self.pos, size=self.size)
+
+
+class OneCircle(Widget):
+    def __init__(self, start_pos: tuple, start_size: tuple, rgb_tuple: tuple, **kwargs):
         super().__init__(**kwargs)
         self.color: tuple = rgb_tuple
 
         self.pos: tuple = start_pos
-        self.size = 50, 50
+        self.size = start_size
+        self.ball_radius = start_size[0] / 2
         self.my_verlet: VerletObject = VerletObject(self.pos, base_gravity, delta_t)
 
         with self.canvas.before:
@@ -52,21 +90,18 @@ class SomeCircle(Widget):
     def UpdatePosition(self):
 
         # Don't really have to set the Widget's position, but the Circle does have to be set, so...
-        self.pos = self.my_verlet.SolveForPosition()
-        self.circle.pos = self.pos
+        self.pos: tuple = self.my_verlet.SolveForPosition()
+        self.circle.pos = (self.pos[0] - self.ball_radius, self.pos[1] - self.ball_radius)
 
 
 class MainPage(RelativeLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.circles = []
+        self.balls = []
+        self.constraints = {}
 
-        some_pos = (500, 500)
-        some_color = (1.0, 0.5, 0.1, 1.0)
-        one_circle = SomeCircle(some_pos, some_color)
-        self.circles.append(one_circle)
-        for a_ball in self.circles:
-            self.add_widget(a_ball)
+        self.add_ball_pit()
+        self.add_balls()
 
         Clock.schedule_interval(self.update_all_circles, 1/60.0)
 
@@ -74,14 +109,52 @@ class MainPage(RelativeLayout):
             Color(0.1, 0.1, 0.1, 1.0)
             self.bg_rect = Rectangle()
 
+    def add_balls(self):
+
+        VerletObject.constraints = pit_constraints
+        ball_radius = pit_constraints['ball_radius']
+
+        some_pos = (700, 500)
+        some_size = (ball_radius * 2, ball_radius * 2)
+        some_color = (1.0, 1.0, 1.0, 1.0)
+        one_circle = OneCircle(some_pos, some_size, some_color)
+        self.balls.append(one_circle)
+
+        some_pos = (800, 750)
+        some_size = (ball_radius * 2, ball_radius * 2)
+        some_color = (1.0, 1.0, 1.0, 1.0)
+        one_circle = OneCircle(some_pos, some_size, some_color)
+        self.balls.append(one_circle)
+
+        for a_ball in self.balls:
+            self.add_widget(a_ball)
+
+    def add_ball_pit(self):
+
+        self.ball_pit = BallPit(pit_constraints)
+        self.add_widget(self.ball_pit)
+
     def on_size(self, instance, new_size):
 
         self.bg_rect.size = self.size
 
     def update_all_circles(self, *args):
 
-        for a_ball in self.circles:
+        collision_list = []
+
+        for a_ball in self.balls:
             a_ball.UpdatePosition()
+
+            for one_ball in self.balls:
+                if a_ball == one_ball:
+                    continue
+                collided, new_vec = a_ball.my_verlet.DetectCollision(one_ball.my_verlet)
+                if collided:
+                    collision_list.append((a_ball, one_ball))
+                    a_ball.my_verlet.position_current += new_vec
+                    one_ball.my_verlet.position_current -= new_vec
+
+        # print(f"{collision_list=}")
 
 
 class canvasMain(App):
--- src/verlet_engine.py
+++ src/verlet_engine.py
@@ -5,8 +5,13 @@ Based on video:
   - https://www.youtube.com/watch?v=lS_qeBy3aQI
 
 This module has the actual Verlet code, and no Kivy code.
+
+Note that Constraints, currently consist of a Circle with point.radius - if
+the Ball objects touch the radius, they're *out*.
 """
 
+import math
+import random
 from dataclasses import dataclass
 
 
@@ -24,6 +29,9 @@ class Vector2:
     def __mul__(self, some_float: float):
         return Vector2(self.x * some_float, self.y * some_float)
 
+    def __truediv__(self, some_float: float):
+        return Vector2(self.x / some_float, self.y / some_float)
+
     def __iter__(self):
         """ This is so a Vector2 can be unpacked into a Tuple easily. It's the equivalent
             of doing 'foo = *(5, 5)'
@@ -39,6 +47,8 @@ class Vector2:
 
 
 class VerletObject:
+    constraints: dict = None
+
     def __init__(self, initial_pos: tuple, base_gravity: Vector2, delta_t: float, **kwargs):
 
         self.position_current: Vector2 = Vector2(*initial_pos)
@@ -51,6 +61,8 @@ class VerletObject:
     def SolveForPosition(self) -> Vector2:
 
         self.ApplyGravity()
+        self.ApplyConstraints()
+        # self.SolveCollisions()
         self.UpdatePosition()
 
         return self.position_current
@@ -58,6 +70,24 @@ class VerletObject:
     def ApplyGravity(self):
 
         self.acceleration += self.base_gravity
+
+    def ApplyConstraints(self):
+
+        widget_position = self.constraints['pit_pos']
+        pit_radius = self.constraints['pit_radius']
+        ball_radius = self.constraints['ball_radius']
+
+        # This is a bit kludgy, turning the radius into a Vector, but it was that, or overload the add-function
+        pit_center: Vector2 = Vector2(*widget_position) + Vector2(pit_radius, pit_radius)
+
+        vector_to_center: Vector2 = self.position_current - pit_center
+
+        dist_to_center = math.hypot(*vector_to_center)
+
+        if dist_to_center + ball_radius > pit_radius:
+            new_vec: Vector2 = vector_to_center / dist_to_center
+            new_pos = pit_center + new_vec * (pit_radius - ball_radius)
+            self.position_current = new_pos
 
     def UpdatePosition(self):
 
@@ -67,3 +97,34 @@ class VerletObject:
         self.position_current = self.position_current + velocity + self.acceleration * self.delta_t * self.delta_t
 
         self.acceleration = Vector2(0.0, 0.0)  # Reset acceleration to zero - not actually sure why...
+
+    def DetectCollision(self, some_other_Verlet) -> (bool, Vector2):
+
+        """
+            VerletObject& object_2 = object_container[k];
+            const Vec2 collision_axis = object_1.position_current - object_2.position_current
+            const float dist = MathVec::length(collision_axis);
+
+            if (dist < 100.0f) {
+                const Vec2 n = collision_axis / dist;
+                const float delta = 100.0f - dist;
+                object_1.position_current += 0.5f * delta * n;
+                object_2.position_current -= 0.5f * delta * n;
+            }
+        """
+
+        object_1 = self
+        object_2 = some_other_Verlet
+        collision_axis: Vector2 = object_1.position_current - object_2.position_current
+        collision_distance: float = math.hypot(*collision_axis)
+        ball_diameter = self.constraints['ball_radius'] * 2
+
+        if collision_distance < ball_diameter:
+            new_vec: Vector2 = collision_axis / collision_distance
+            new_delta: float = ball_diameter - collision_distance
+            big_delta: Vector2 = new_vec * new_delta * 0.9
+
+            # print(f"{collision_distance=}, {big_delta=}")
+            return True, big_delta
+
+        return False, None
